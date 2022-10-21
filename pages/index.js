10c93642d13574b28553674f8d3e3d760b132db7
zohaibzyebi <zyebi553@gmail.com>
2022-10-22 00:05:39 +0500

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,8 @@
-import { createTheme, ThemeProvider } from "@mui/material/styles";
-
-const theme = createTheme({
-  // breakpoints: {
-  //   values: {
-  //     xs: 0,
-  //     sm: 600,
-  //     md: 900,
-  //     lg: 1350,
-  //     lgBig: 1450,
-  //     xl: 1500,
-  //     xxl: 1700,
-  //   },
-  // },
-});
+import Homepage from '../components/Main/Homepage'
 
 function index() {
   return (
-    <div style={{fontFamily:'AvenirNextLTPro',fontWeight:800,fontStyle:'normal'}} >index</div>
+    <Homepage />
   )
 }
 
--- pages/layout.js
+++ pages/layout.js
@@ -1,7 +1,23 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1350,
+      xl: 1500,
+    },
+  },
+});
+
 export default function Layout({ children }) {
   return (
     <>
-      <main>{children}</main>
+      <ThemeProvider theme={theme} >
+        <main>{children}</main>
+      </ThemeProvider>
     </>
   )
 }
